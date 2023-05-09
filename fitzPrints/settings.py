0100e119660ddf019fec7f07c22af365751acd79
jamitz440 <113603804+jamitz440@users.noreply.github.com>
2023-05-09 12:56:06 +0000

    setup for railway

--- fitzPrints/settings.py
+++ fitzPrints/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+e&h19#k(y6t^7r+(b7@fe+2uf^)*u0h%$_gozslxj3oaw7qpb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -149,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

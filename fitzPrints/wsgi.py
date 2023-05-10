68410125286d98eb1848c04e6adfb3a117997b92
godd0t <lirrishala@gmail.com>
2023-05-10 23:19:17 +0200

    fix: update .gitignore && load settings from env

--- fitzPrints/wsgi.py
+++ fitzPrints/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitzPrints.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitzPrints.settings.prod')
 
 application = get_wsgi_application()
